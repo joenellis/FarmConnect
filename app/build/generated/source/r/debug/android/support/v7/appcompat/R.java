41a0d2135eeaa14345ba8bcb0ad9e287320cf335
CodeLord <abiboy55@gmail.com>
2017-12-09 20:06:23 +0000

    Did the map intent

--- app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -677,7 +677,7 @@ public final class R {
         public static final int topPanel = 0x7f09011f;
         public static final int uniform = 0x7f09012c;
         public static final int up = 0x7f09012d;
-        public static final int wrap_content = 0x7f09014e;
+        public static final int wrap_content = 0x7f090150;
     }
     public static final class integer {
         public static final int abc_config_activityDefaultDur = 0x7f0a0000;
