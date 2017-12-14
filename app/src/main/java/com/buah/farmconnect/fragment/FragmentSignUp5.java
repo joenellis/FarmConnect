e3082a54e9b5e101f15b76b2410a68b9ed6a3cb7
CodeLord <abiboy55@gmail.com>
2017-12-14 18:05:23 +0000

    Fixed the Security spinner for sign up.

--- app/src/main/java/com/buah/farmconnect/fragment/FragmentSignUp5.java
+++ app/src/main/java/com/buah/farmconnect/fragment/FragmentSignUp5.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.buah.farmconnect.R;
 
@@ -22,7 +24,19 @@ public class FragmentSignUp5 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_5, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up_5, container, false);
+
+        Spinner securityQuestion = view.findViewById(R.id.forgotPassword_securityQuestion);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getContext(),
+                R.array.security_questions,
+                android.R.layout.simple_list_item_1
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        securityQuestion.setAdapter(adapter);
+
+        return view;
     }
 
 }
