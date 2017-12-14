57670960bb9d8bdabba6ad01cd177116189670ca
LunarKareem <LunarKareem@users.noreply.github.com>
2017-12-14 13:02:11 +0000

    UI design and implementation for: 1. Forgot Password, 2. Edit Password and 3. Security Question.

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -998,13 +998,13 @@ public final class R {
         public static final int appbar_scrolling_view_behavior = 0x7f0e001f;
         public static final int bottom_sheet_behavior = 0x7f0e0020;
         public static final int character_counter_pattern = 0x7f0e0021;
-        public static final int password_toggle_content_description = 0x7f0e004f;
-        public static final int path_password_eye = 0x7f0e0050;
-        public static final int path_password_eye_mask_strike_through = 0x7f0e0051;
-        public static final int path_password_eye_mask_visible = 0x7f0e0052;
-        public static final int path_password_strike_through = 0x7f0e0053;
-        public static final int search_menu_title = 0x7f0e0058;
-        public static final int status_bar_notification_info_overflow = 0x7f0e005b;
+        public static final int password_toggle_content_description = 0x7f0e0050;
+        public static final int path_password_eye = 0x7f0e0051;
+        public static final int path_password_eye_mask_strike_through = 0x7f0e0052;
+        public static final int path_password_eye_mask_visible = 0x7f0e0053;
+        public static final int path_password_strike_through = 0x7f0e0054;
+        public static final int search_menu_title = 0x7f0e0059;
+        public static final int status_bar_notification_info_overflow = 0x7f0e005c;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat = 0x7f0f0000;
